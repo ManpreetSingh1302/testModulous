a4f57ad1c9bd71557447721140d02091e23fa197
Manpreet Singh <mpreetdhiman1@gmail.com>
2025-02-20 18:17:04 +0530

    removed styles from main file

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -11,6 +11,7 @@ import {
 import React, {memo, useState, useRef} from 'react';
 import Video from 'react-native-video';
 import {SECTIONS_DATA} from '../../constants/constants';
+import {styles} from './HomeScreenStyles';
 
 const {width} = Dimensions.get('window');
 
@@ -161,65 +162,3 @@ const HorizontalMediaList = memo(
 );
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: '#fff',
-    paddingBottom: 20,
-    paddingTop: 10,
-  },
-  sectionHeader: {
-    fontSize: 18,
-    fontWeight: '600',
-    padding: 15,
-    backgroundColor: '#fff',
-  },
-  horizontalList: {
-    paddingHorizontal: 0,
-  },
-  media: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#f0f0f0',
-    borderRadius: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  cardContainer: {
-    width: width,
-    paddingHorizontal: 15,
-    marginTop: 25,
-  },
-  mediaItem: {
-    height: 250,
-    backgroundColor: 'white',
-    borderRadius: 12,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 3,
-    overflow: 'hidden',
-    marginVertical: 2,
-  },
-  pagination: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  paginationDot: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: '#ccc',
-    marginHorizontal: 4,
-  },
-  paginationDotActive: {
-    backgroundColor: '#000',
-  },
-});
